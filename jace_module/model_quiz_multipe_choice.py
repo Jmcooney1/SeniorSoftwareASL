7d0db0c6e7da92a0070ce11eac97401696a9e597
Jace Cooney <marymargaretcooney39@gmail.com>
2026-04-12 13:36:35 -0500

    got the multipe choice to have words, with temparpory question

--- jace_module/model_quiz_multipe_choice.py
+++ jace_module/model_quiz_multipe_choice.py
@@ -13,10 +13,18 @@ class QuizInputWidget(QWidget):
         self.setWindowTitle("Quiz Input - Multiple Choice")
         self.setGeometry(100, 100, 500, 500)
 
+        self.answers = {
+            "A": "Cat",
+            "B": "Dog",
+            "C": "Bird",
+            "D": "Fish"
+        }
+
+        self.correct_answer = "Cat"
+
         main_layout = QVBoxLayout()
         main_layout.setSpacing(15)
 
-        # --- 3D MODEL PLACEHOLDER ---
         self.model_box = QFrame()
         self.model_box.setFixedHeight(220)
         self.model_box.setStyleSheet("""
@@ -30,31 +38,41 @@ class QuizInputWidget(QWidget):
         model_label = QLabel("3D Model Placeholder")
         model_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         model_layout.addWidget(model_label)
-
         main_layout.addWidget(self.model_box)
 
-        # --- QUESTION BOX ---
         self.question_box = QFrame()
         self.question_box.setStyleSheet("""
             QFrame {
                 border: 2px solid black;
                 background-color: white;
             }
+            QRadioButton {
+                color: black;
+                font-size: 15px;
+                padding: 4px;
+            }
+            QLabel {
+                color: black;
+                font-size: 16px;
+            }
         """)
 
         question_layout = QVBoxLayout(self.question_box)
 
         self.question_label = QLabel("Question: What sign is this?")
-        self.question_label.setStyleSheet("font-size: 16px;")
         question_layout.addWidget(self.question_label)
 
-        # --- MULTIPLE CHOICE OPTIONS ---
         self.button_group = QButtonGroup(self)
 
-        self.option_a = QRadioButton("A")
-        self.option_b = QRadioButton("B")
-        self.option_c = QRadioButton("C")
-        self.option_d = QRadioButton("D")
+        self.option_a = QRadioButton(f"A. {self.answers['A']}")
+        self.option_b = QRadioButton(f"B. {self.answers['B']}")
+        self.option_c = QRadioButton(f"C. {self.answers['C']}")
+        self.option_d = QRadioButton(f"D. {self.answers['D']}")
+
+        self.option_a.setProperty("answer", self.answers["A"])
+        self.option_b.setProperty("answer", self.answers["B"])
+        self.option_c.setProperty("answer", self.answers["C"])
+        self.option_d.setProperty("answer", self.answers["D"])
 
         self.button_group.addButton(self.option_a)
         self.button_group.addButton(self.option_b)
@@ -68,22 +86,29 @@ class QuizInputWidget(QWidget):
 
         main_layout.addWidget(self.question_box)
 
-        # --- SUBMIT BUTTON ---
         self.submit_button = QPushButton("Submit")
         self.submit_button.clicked.connect(self.check_answer)
         main_layout.addWidget(self.submit_button)
+
+        self.result_label = QLabel("")
+        self.result_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        main_layout.addWidget(self.result_label)
 
         self.setLayout(main_layout)
 
     def check_answer(self):
         selected_button = self.button_group.checkedButton()
 
-        if selected_button:
-            answer = selected_button.text()
-            print("Selected answer:", answer)
-        else:
-            print("No option selected")
+        if not selected_button:
+            self.result_label.setText("No option selected")
+            return
 
+        answer = selected_button.property("answer")
+
+        if answer == self.correct_answer:
+            self.result_label.setText("Correct!")
+        else:
+            self.result_label.setText("Wrong!")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
